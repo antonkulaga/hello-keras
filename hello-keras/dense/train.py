9ca0cda3eeb4eebd6733126ac2ea9029ec3836b8
antonkulaga <antonkulaga@gmail.com>
2019-04-19 02:40:13 +0300

    added convolution notebook

--- hello-keras/dense/train.py
+++ hello-keras/dense/train.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from keras.models import Sequential
 from keras.layers.core import Dense
+from keras.layers import Dropout
 from keras.optimizers import SGD
 from imutils import paths
 import matplotlib.pyplot as plt
@@ -17,26 +18,23 @@ import random
 import pickle
 import cv2
 import os
+cwd = os.getcwd()
+data = "/data/training/hello-keras"
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-cwd = os.getcwd()
-
-ap.add_argument("-d", "--dataset",
-                default="/data/training/hello-keras",
-                required=False, help="path to input dataset of images")
-
-ap.add_argument("-m", "--model",
-                default=os.path.join(cwd,".model"),
-                required=False,
-                help="path to output trained model")
-
-ap.add_argument("-l", "--label-bin", required=False,
-                default=os.path.join(cwd,"output", "labels"),
-                help="path to output label binarizer")
-
-ap.add_argument("-p", "--plot", required=False,
-                default=os.path.join(cwd,"output", "plots"),
+ap.add_argument("-d", "--dataset", required=False, default=data,
+                help="path to input dataset of images")
+ap.add_argument("-p", "--plot", required=False, default=os.path.join(data, "plot"),
                 help="path to output accuracy/loss plot")
-
+ap.add_argument("-m", "--model", required=False, default=os.path.join(data, "dense.h5"),
+                help="path to output trained model")
+ap.add_argument("-l", "--label-bin", required=False, default=os.path.join(data, "labels.pkl"),
+                help="path to output label binarizer")
 args = vars(ap.parse_args())
+
+
+# grab the image paths and randomly shuffle them
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
